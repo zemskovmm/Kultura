3f5253447ec459b9f976ce3ab2dbfd1b46525983
Michael <dreamwalkerzmm@gmail.com>
2023-04-19 23:18:48 +0200

    Initial commit

--- Kultura/ViewController.swift
+++ Kultura/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var frame = view.frame
+
+        // Adjust the frame's Y position
+        frame.origin.y += 50
+        // Create the WKWebView instance
+        webView = WKWebView(frame: frame)
+        
+        // Add the WKWebView as a subview of the main view
+        view.addSubview(webView)
+        
+        if let url = URL(string: "https://appevent.ru/w/5038/?fbclid=PAAab-L7uQ6arMZBowwJSlW-spP3pisNqvEfjB-oBLhCfTL97tPb0mqPFL8tU") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-
-
 }
 
